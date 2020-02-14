b295f719a110d984f9734cee3c48470e468cfc72
张显磊 <1079105171@qq.com>
2020-02-14 16:16:47 +0800

    完成留言板功能

--- src/page/page4/page4.jsx
+++ src/page/page4/page4.jsx
@@ -15,23 +15,28 @@ class Page4 extends React.Component {
         messageList: [],
         showLoading: true,
         replyCommentText: '',
-        showReplyCommentIpt: false,
+        showReplyCommentIptIndex: NaN,
         nameIptValue: '',
-        showIptValueSugget: false,
         textareaValue: '',
         diableSubBtn: false
     }
 
+    // 产生随机名字
+    creatRandomName() {
+        const name = localStorage.getItem('nameIptValue') || '访客' + Math.floor(Math.random() * 1000 + 1000);
+        this.setState({ nameIptValue: name });
+    }
+
     // 获取留言列表数据
     async getMseeageList() {
-        const data = await (await fetch('http://localhost:3000/myWebSite/getComments', { mode: 'cors' })).json();
+        const data = await (await fetch('http://114.55.28.254/:3000/myWebSite/getComments', { mode: 'cors' })).json();
         this.setState({ messageList: JSON.parse(data).reverse(), showLoading: false });
     }
 
 
     // 渲染留言列表
     renderMessageList = () => {
-        const { messageList, replyCommentText, showReplyCommentIpt } = this.state;
+        const { messageList, replyCommentText, showReplyCommentIptIndex } = this.state;
         return messageList.map((item, index) => {
             return <Comment
                 key={index}
@@ -54,7 +59,7 @@ class Page4 extends React.Component {
                         </span>,
 
                         // 回复按钮 这是三元表达式
-                        showReplyCommentIpt ?
+                        showReplyCommentIptIndex === index ?
                             <div>
                                 <input
                                     value={replyCommentText}
@@ -62,31 +67,39 @@ class Page4 extends React.Component {
                                     onKeyPress={e => e.charCode === 13 && this.replyComment(index)}
                                     placeholder="请输入回复内容"
                                 />
-                                <span onClick={() => this.replyComment()} className='submitReplyBtn'>确定</span>
-                                <span onClick={() => this.setState({ showReplyCommentIpt: false })} className='cancelReplyBtn'>取消</span>
+                                <span onClick={() => this.replyComment(index)} className='submitReplyBtn'>确定</span>
+                                <span onClick={() => this.setState({ showReplyCommentIptIndex: NaN })} className='cancelReplyBtn'>取消</span>
                             </div>
-                            : <span onClick={() => this.setState({ showReplyCommentIpt: true })} className='replyCommentBtn'>回复</span>
+                            : <span onClick={() => this.setState({ showReplyCommentIptIndex: index })} className='replyCommentBtn'>回复</span>
                     ]
                 }
                 author={item.name}
                 avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" alt="Han Solo" />}
                 content={<p className='commentContent'>{item.content}</p>}
                 datetime={item.createTime}
+                children={item.comments.length && item.comments.map(itemSon =>
+                    <Comment
+                        key={itemSon.name}
+                        author={itemSon.name}
+                        datetime={itemSon.createTime}
+                        content={<p className='commentContent'>{itemSon.content}</p>}
+                        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" alt="Han Solo" />}
+                    />
+                )}
             />
         })
     }
 
     // 留言点赞
     async praiseComment(index) {
-        const { messageList } = this.state;
+        const { messageList, replyCommentText } = this.state;
         // 发送点赞请求
-        const result = JSON.parse(await (await fetch('http://localhost:3000/myWebSite/praise', {
+        const result = JSON.parse(await (await fetch('http://114.55.28.254/:3000/myWebSite/praise', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ id: messageList[index]._id }),
+            body: JSON.stringify({ id: messageList[index]._id, content: replyCommentText }),
             mode: 'cors'
         })).json());
-
         // 如果请求成功
         if (result.status === 200) {
             messageList[index].praise += 1;
@@ -98,13 +111,12 @@ class Page4 extends React.Component {
     async disApproveComment(index) {
         const { messageList } = this.state;
         // 发送踩一下请求
-        const result = JSON.parse(await (await fetch('http://localhost:3000/myWebSite/disapprove', {
+        const result = JSON.parse(await (await fetch('http://114.55.28.254/:3000/myWebSite/disapprove', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ id: messageList[index]._id }),
             mode: 'cors'
         })).json());
-
         // 如果请求成功
         if (result.status === 200) {
             messageList[index].disapprove += 1;
@@ -112,28 +124,52 @@ class Page4 extends React.Component {
         }
     }
 
-    replyComment() {
+    // 回复留言
+    async replyComment(index) {
+        // 关闭输入框
+        this.setState({ showReplyCommentIptIndex: NaN });
+        const { messageList, nameIptValue, replyCommentText } = this.state;
+        if (replyCommentText.trim() === '') {
+            return window.alert('输入的内容不能为空!')
+        }
+        if (!nameIptValue.trim()) {
+            await this.creatRandomName();
+        }
+        // 发送请求
+        const result = JSON.parse(await (await fetch('http://114.55.28.254/:3000/myWebSite/replyComment', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ id: messageList[index]._id, content: replyCommentText, name: nameIptValue }),
+            mode: 'cors'
+        })).json());
 
+        if (result.status === 200) {
+            this.getMseeageList();
+        }
     }
 
     // 提交留言
     async submitComment() {
         const { nameIptValue, textareaValue } = this.state;
+        // 如果名字为空产生随机名字
+        if (!nameIptValue.trim()) {
+            await this.creatRandomName();
+        }
+        // 如果留言内容为空 直接返回
+        if(!textareaValue.trim()){
+            return window.alert('内容不能为空！')
+        }
         // 禁用提交按钮
         this.setState({ diableSubBtn: true });
-        const result =JSON.parse(await (await fetch('http://localhost:3000/myWebSite/submitComment', {
+        const result = JSON.parse(await (await fetch('http://114.55.28.254/:3000/myWebSite/submitComment', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ name: nameIptValue, content: textareaValue }),
             mode: 'cors'
         })).json());
-
-
         if (result.status === 200) {
             this.getMseeageList();
-            console.dir(textareaRef.current);
             textareaRef.current.value = '';
-            this.renderMessageList();
         }
         // 恢复提交按钮
         this.setState({ diableSubBtn: false });
@@ -142,17 +178,18 @@ class Page4 extends React.Component {
 
     componentDidMount() {
         this.getMseeageList();
+        this.creatRandomName();
     }
 
 
 
     render() {
-        const { showLoading, nameIptValue, diableSubBtn,showIptValueSugget } = this.state;
+        const { showLoading, nameIptValue, diableSubBtn } = this.state;
         return <div className='page4Container'>
+            <div className='messageBoardTitle'>留言板</div>
 
             {/* 留言板展示列表 */}
             <div className='messageBoard' id='messageBoard'>
-                <div className='messageBoardTitle'>留言板</div>
                 <div className='messageList'>
                     {showLoading ? ShowLoading('留言板加载中...') : this.renderMessageList()}
                 </div>
@@ -165,16 +202,17 @@ class Page4 extends React.Component {
                 <div className='leaveMessageFormName'>
                     <span>姓名(昵称):</span>
                     <input
+                        value={nameIptValue}
                         placeholder='请输入任意字符作为姓名'
                         onFocus={e => e.target.style.backgroundColor = "#000"}
                         onBlur={e => e.target.style.backgroundColor = "transparent"}
-                        onChange={e => this.setState({ nameIptValue: e.target.value, showIptValueSugget: true })}
+                        onChange={e => { this.setState({ nameIptValue: e.target.value }); localStorage.setItem('nameIptValue', e.target.value); }}
                     ></input>
                 </div>
 
 
                 <div className='leaveMessageFormWarm'>
-                    {/^[\u4e00-\u9fa5a-zA-Z0-9]{3,15}$/.test(nameIptValue) ? null : showIptValueSugget ? '请输入3-15位的汉字,英文或数字' : null}
+                    {/^[\u4e00-\u9fa5a-zA-Z0-9]{3,15}$/.test(nameIptValue) ? null : '请输入3-15位的汉字,英文或数字'}
                 </div>
 
                 <div className='leaveMessageFormContext'>
